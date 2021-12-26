bbbe951bb35f929eed2f5d0536823c2c975beb8e
Lunes RPG <93552894+lunesrpg@users.noreply.github.com>
2021-12-26 04:07:17 +0000

    Add files via upload

---
+++ js/extras/senha.js
@@ -0,0 +1,8 @@
+senha = 'fichaexemplo';
+senhadig = prompt("A senha é: fichaexemplo","")
+if (senha != senhadig){
+alert('Senha incorreta! Redirecionando...');
+top.location.href='fichas.html';
+}
+
+// https://obfuscator.io/
